f65777e9ddbdd2d68fe16a782dcba69bb8bf2723
tzaeru <tzaeru@gmail.com>
2017-09-18 21:09:09 +0300

    initial

---
+++ Assets/StickthingController.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickthingController : MonoBehaviour {
+    public GameObject middle;
+    public GameObject left_leg;
+    public GameObject right_leg;
+
+    public float impulse_force;
+
+    public bool ai_controlled = true;
+
+    public StickthingNeuralNet neural;
+
+    public float time_per_run = 10.0f;
+
+    public float timescale = 100.0f;
+
+    float last_impulsed = 0.0f;
+    public float impulse_rate = 4.0f;
+
+    Vector3[] original_positions = new Vector3[3];
+    float[] original_rotations = new float[3];
+    
+    float start_time;
+
+    float record = 0.0f;
+    int generations = 0;
+
+    public UnityEngine.UI.Text generations_text;
+    public UnityEngine.UI.Text record_text;
+    public UnityEngine.UI.Text last_result_text;
+
+    // Use this for initialization
+    void Start () {
+        if (ai_controlled)
+            Time.timeScale = timescale;
+
+        start_time = Time.time;
+
+        original_positions[0] = middle.transform.position;
+        original_positions[1] = left_leg.transform.position;
+        original_positions[2] = right_leg.transform.position;
+
+        original_rotations[0] = middle.transform.rotation.eulerAngles.z;
+        original_rotations[1] = left_leg.transform.rotation.eulerAngles.z;
+        original_rotations[2] = right_leg.transform.rotation.eulerAngles.z;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (ai_controlled && (Time.time - last_impulsed) > 1.0f/impulse_rate && Time.time - start_time > 1.0f)
+        {
+            float left_angle = left_leg.GetComponent<Rigidbody2D>().rotation;
+            float right_angle = right_leg.GetComponent<Rigidbody2D>().rotation;
+
+            float height_diff = left_leg.transform.position.y - right_leg.transform.position.y;
+
+            float[] torques = neural.GetTorques(left_angle, right_angle, height_diff);
+
+            left_leg.GetComponent<Rigidbody2D>().AddTorque((torques[0] - 0.5f) * 2.0f * impulse_force, ForceMode2D.Impulse);
+            right_leg.GetComponent<Rigidbody2D>().AddTorque((torques[1] - 0.5f) * 2.0f * impulse_force, ForceMode2D.Impulse);
+
+            last_impulsed = Time.time;
+        }
+        else
+        {
+            if (Input.GetKeyDown("a"))
+            {
+                left_leg.GetComponent<Rigidbody2D>().AddTorque(impulse_force, ForceMode2D.Impulse);
+            }
+            else if (Input.GetKeyDown("d"))
+            {
+                left_leg.GetComponent<Rigidbody2D>().AddTorque(-impulse_force, ForceMode2D.Impulse);
+            }
+
+            if (Input.GetKeyDown("left"))
+            {
+                right_leg.GetComponent<Rigidbody2D>().AddTorque(impulse_force, ForceMode2D.Impulse);
+            }
+            else if (Input.GetKeyDown("right"))
+            {
+                right_leg.GetComponent<Rigidbody2D>().AddTorque(-impulse_force, ForceMode2D.Impulse);
+            }
+        }
+
+        // Reset position
+        if (Time.time - start_time > time_per_run)
+        {
+            float result = middle.transform.localPosition.x;
+
+            middle.transform.position = original_positions[0];
+            left_leg.transform.position = original_positions[1];
+            right_leg.transform.position = original_positions[2];
+
+            middle.transform.rotation = Quaternion.Euler(0.0f, 0.0f, original_rotations[0]);
+            left_leg.transform.rotation = Quaternion.Euler(0.0f, 0.0f, original_rotations[1]);
+            right_leg.transform.rotation = Quaternion.Euler(0.0f, 0.0f, original_rotations[2]);
+
+            middle.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 0.0f);
+            left_leg.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 0.0f);
+            right_leg.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 0.0f);
+
+            middle.GetComponent<Rigidbody2D>().angularVelocity = 0.0f;
+            left_leg.GetComponent<Rigidbody2D>().angularVelocity = 0.0f;
+            right_leg.GetComponent<Rigidbody2D>().angularVelocity = 0.0f;
+
+            start_time = Time.time;
+
+            // Apply our amazing learning.
+            neural.ModifyFromResult(result);
+
+            if (result > record)
+                record = result;
+            generations++;
+
+            generations_text.text = "Generations: " + generations;
+            last_result_text.text = "Last Result: " + result;
+            record_text.text = "Record: " + record;
+        }
+    }
+}
---
+++ Assets/StickthingNeuralNet.cs
@@ -0,0 +1,123 @@
+﻿using FANNCSharp;
+using FANNCSharp.Float;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+using DataType = System.Single;
+
+public class StickthingNeuralNet : MonoBehaviour {
+
+    public NeuralNet net;
+
+    float last_result;
+
+    uint last_from_neuron;
+    uint last_to_neuron;
+    float last_weight;
+
+    bool first_time_modifying = true;
+
+    // Use this for initialization
+    void Awake () {
+        Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Init()
+    {
+        const uint num_layers = 3;
+        const uint num_neurons_hidden = 3;
+
+        // Inputs: Angle AB, height difference
+        net = new NeuralNet(NetworkType.LAYER, num_layers, 3, num_neurons_hidden, 2);
+
+        net.RandomizeWeights(-1.0f, 1.0f);
+
+        print("Connection count: " + net.ConnectionArray.Length);
+        for (int i = 0; i < net.ConnectionArray.Length; i++)
+        {
+            //print("Weight " + i.ToString() + ": " + net.ConnectionArray[i].Weight);
+        }
+    }
+
+    public float[] GetTorques(float angle_left, float angle_right, float height_difference)
+    {
+        return net.Run(new float[] { angle_left, angle_right, height_difference });
+    }
+
+    public void ModifyFromResult(float result)
+    {
+        // If it's not first time modifying & result is worse than last result, return modifications
+        if (!first_time_modifying && result < last_result)
+        {
+            print("Worse result.");
+            net.SetWeight(last_from_neuron, last_to_neuron, last_weight);
+        }
+        else
+        {
+            print("Better result.");
+        }
+
+        // Randomize some weight.
+        uint random_connection = (uint)UnityEngine.Random.Range(0, (int)net.ConnectionArray.Length);
+
+        last_from_neuron = net.ConnectionArray[random_connection].FromNeuron;
+        last_to_neuron = net.ConnectionArray[random_connection].ToNeuron;
+        last_weight = net.ConnectionArray[random_connection].Weight;
+
+        net.SetWeight(net.ConnectionArray[random_connection].FromNeuron, net.ConnectionArray[random_connection].ToNeuron, UnityEngine.Random.Range(-1.0f, 1.0f));
+
+        last_result = result;
+
+        first_time_modifying = false;
+    }
+    /*
+    static StreamReader trainingFile = null;
+    static StreamReader testFile = null;
+    static void TrainingDataCallback(uint number, uint inputCount, uint outputCount, DataType[] input, DataType[] output)
+    {
+        print("input count: " + inputCount);
+
+        float[] data = new float[] {UnityEngine.Random.Range(0.0f, 1.0f), UnityEngine.Random.Range(0.0f, 1.0f)};
+
+        for (int i = 0; i < 2; i++)
+        {
+            input[i] = data[i];
+        }
+
+        output[0] = 1.0f;
+    }
+
+    static void TestDataCallback(uint number, uint inputCount, uint outputCount, DataType[] input, DataType[] output)
+    {
+        print("input count: " + inputCount);
+
+        float[] data = new float[] { UnityEngine.Random.Range(0.0f, 1.0f), UnityEngine.Random.Range(0.0f, 1.0f) };
+
+        for (int i = 0; i < 2; i++)
+        {
+            input[i] = data[i];
+        }
+
+        output[0] = 1.0f;
+    }
+
+    static int TrainingCallback(NeuralNet net, TrainingData data, uint maxEpochs, uint epochsBetweenReports, float desiredError, uint epochs, object userData)
+    {
+        print(String.Format("CAAAAAAAAALLLBAAAAAAAACK: MSE error on train data: {0}", net.TestData(data)));
+        System.GC.Collect(); // Make sure nothing's getting garbage-collected prematurely
+        GC.WaitForPendingFinalizers();
+        print(String.Format("Callback: Last neuron weight: {0}, Last data input: {1}, Max epochs: {2}\nEpochs between reports: {3}, Desired error: {4}, Current epoch: {5}\nGreeting: \"{6}\"",
+                            net.ConnectionArray[net.TotalConnections - 1].Weight, data.InputAccessor.Get((int)data.TrainDataLength - 1, (int)data.InputCount - 1),
+                            maxEpochs, epochsBetweenReports, desiredError, epochs, userData));
+        return 1;
+    }
+    */
+}
